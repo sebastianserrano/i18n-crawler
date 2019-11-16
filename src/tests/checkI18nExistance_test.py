9254142bbe4e6897ac5217162aa3639be63ed3c3
Sebastian Serrano <sebastian@tactable.io>
2019-11-15 23:17:34 -0500

    RENAMED put under tests dir

--- src/tests/checkI18nExistance_test.py
+++ src/tests/checkI18nExistance_test.py
@@ -1,47 +1,46 @@
-# content of test_sample.py
+""" Regex testing """
+from grappa import expect
 from src import index
 
 class Testi18nRegex:
     def testI18nMatchesIsolatedLine(self):
         line = 'i18n.t("settings.expiryBanner.expired")'
         match = index.checkI18nExistance(line)
-        assert match
+        bool(match) | expect.to.be.true
 
     def testRegularLine(self):
-        lineOne = 'messages: [i18n.t("productSync.error")],'
-        lineTwo = 'content: i18n.t("settings.expiryBanner.updateRMSKey"),'
+        lines = ['messages: [i18n.t("productSync.error")],',
+                 'content: i18n.t("settings.expiryBanner.updateRMSKey"),']
 
-        matchOne = index.checkI18nExistance(lineOne)
-        matchTwo = index.checkI18nExistance(lineTwo)
-
-        assert match
+        results = [bool(index.checkI18nExistance(x)) for x in lines]
+        results | expect.to_not.contain(False)
 
     def testI18nWithMultipleParameters(self):
         line = 'string += i18n.t("settings.expiryBanner.titleDays", daysToExpiry) + " "'
         match = index.checkI18nExistance(line)
-        assert match
+        bool(match) | expect.to.be.true
 
     def testI18nDoesNotMatchWithoutOpeningParenthesis(self):
         line = 'i18n.t"settings.expiryBanner.expired")'
         match = index.checkI18nExistance(line)
-        assert not match
+        bool(match) | expect.to.be.false
 
     def testI18nDoesNotMatchWithoutClosingParenthesis(self):
         line = 'i18n.t("settings.expiryBanner.expired"'
         match = index.checkI18nExistance(line)
-        assert not match
+        bool(match) | expect.to.be.false
 
     def testI18nDoesNotMatchWithoutOpeningQuotes(self):
         line = 'i18n.t(settings.expiryBanner.expired")'
         match = index.checkI18nExistance(line)
-        assert not match
+        bool(match) | expect.to.be.false
 
     def testI18nDoesNotMatchWithoutClosingQuotes(self):
         line = 'i18n.t("settings.expiryBanner.expired)'
         match = index.checkI18nExistance(line)
-        assert not match
+        bool(match) | expect.to.be.false
 
     def testI18nDoesNotMatchWithoutOpeningNecessaryi18n_tCall(self):
         line = '("settings.expiryBanner.expired")'
         match = index.checkI18nExistance(line)
-        assert not match
+        bool(match) | expect.to.be.false
